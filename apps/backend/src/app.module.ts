1e25fc30540aef333ca716b97999ce1b02d2ce82
amercehic <cehic.am@gmail.com>
2025-07-18 23:09:39 +0200

    Update backend and frontend configurations: modify package.json for production start script, enhance tsconfig.json with path aliases, and adjust import statements for module resolution. Additionally, update Vite configuration to support path aliasing in the frontend.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { validate } from './config/env';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { validate } from '@/config/env';
 
 @Module({
   imports: [
